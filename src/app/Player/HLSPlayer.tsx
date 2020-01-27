bbb85b6f367235601befa208288ec26acd1fd628
muyuu <anticyborg@gmail.com>
2020-01-27 17:26:44 +0900

    put events

--- src/app/Player/HLSPlayer.tsx
+++ src/app/Player/HLSPlayer.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { videoTagEvents } from './video_tag_events';
 
 interface HLSPlayerProps {
     playlist: string;
 }
 
-export const HLSPlayer: React.FC<HLSPlayerProps> = ({ playlist }) => {
+export function HLSPlayer({ playlist }: HLSPlayerProps) {
     const m3u8 = playlist === '' ? null : playlist;
+    const videoRef = useRef<null | HTMLVideoElement>(null);
+
+    useEffect(()=>{
+        const videoTag = videoRef.current;
+        videoTagEvents.forEach((name)=>{
+            videoTag?.addEventListener(name, (e: any)=>{
+                console.debug('video tag Event: %s %O', e.type, e);
+            });
+        });
+    }, [m3u8]);
 
     if (m3u8 === null) {
         return null;
@@ -13,7 +24,7 @@ export const HLSPlayer: React.FC<HLSPlayerProps> = ({ playlist }) => {
 
     return (
         <div key={m3u8}>
-            <video src={m3u8} controls/>
+            <video src={m3u8} controls ref={videoRef} />
         </div>
     );
 };
